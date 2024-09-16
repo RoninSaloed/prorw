7173e2241141ba9b63a53c8b97c75ae51a0dedf6
Богдан Билятинский <bodyagoo@gmail.com>
2024-09-16 11:24:38 +0300

    start project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <p class="text-red-600">test</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
